6f5f69c8205776b2f14fb9dd03dbc4df87aeebe2
shaunaks-tech <45121742+shaunaks-tech@users.noreply.github.com>
2022-07-18 19:44:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-board = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
-]
-for row in board:
-    print(row)
+def print_board():
+    print("Board Printing")
+
+    board = [
+        ["-","-","-"],
+        ["-","-","-"],
+        ["-","-","-"]
+    ]
+    for row in board:
+        print(row)
+print_board()
